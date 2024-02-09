49feef5a552ff70bfcd944a690afc12098faf641
hasanbadalli <120056145+hasanbadalli@users.noreply.github.com>
2024-02-09 21:23:53 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React_Random_Number_Creator
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hasanbadalli/React_Random_Number_Creator)
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function App() {
+  const [minVal, setminVal] = useState(0);
+  const [maxVal, setmaxVal] = useState(10);
+  const [randomNum, setrandomNum] = useState(5);
+  function getMax(event){
+    setmaxVal(+event.target.value);
+  }
+  function getMin(event){
+    setminVal(+event.target.value);
+  }
+  function getRandom(){
+    setrandomNum(Math.floor(Math.random() * (maxVal-minVal+1))+minVal);
+    console.log(minVal);
+    console.log(maxVal);
+  }
+  return (
+    <div>
+      <div className="container">
+        <div className="randomNum">
+          <p>Random Number: {randomNum}</p>
+        </div>
+        <div className="minmaxNum">
+          <div className="min">
+            <p>Minimum Number:</p>
+            <input onChange={getMin} type="number" placeholder={minVal} value={minVal}/>
+          </div>
+          <div className="max">
+            <p>Maximum Number:</p>
+            <input onChange={getMax} type="number" placeholder={maxVal} value={maxVal}/>
+          </div>
+          <button onClick={getRandom}>Get Random Number</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
